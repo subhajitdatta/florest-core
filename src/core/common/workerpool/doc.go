ab1de6748dd6f3bfda742555b9df265b2a28dd2a
Debraj Manna <debraj.manna@jabong.com>
2016-09-24 18:31:06 +0530

    Add documentation for logger and workerpool package

--- src/core/common/workerpool/doc.go
+++ src/core/common/workerpool/doc.go
@@ -1,2 +1,13 @@
-// Package workerpool provides worker pooling solution. Worker pools can be created using this package.
+// Package workerpool provides a buffered channel of workers that can be used to perform
+// task asynchronously in the background.
+//
+// NOTE
+//
+// 1. taskqueue internally uses buffered channel. So when the task queue is already full
+// and if the caller is trying to submit a new task, then the caller will be blocked.
+//
+// 2. workerpool is mainly designed to execute task in the background asynchronously. So
+// right now, it is not possible to return some value (including errors) to the job submitter.
+//
+// Refer https://github.com/jabong/florest-core/wiki/Worker-Pool for more details
 package workerpool
---
+++ src/core/common/workerpool/doc_test.go
@@ -0,0 +1,32 @@
+package workerpool_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jabong/florest-core/src/core/common/workerpool"
+)
+
+type Hello struct {
+}
+
+func (h *Hello) DisplayMsg(msg string) {
+	fmt.Println("Hello " + msg)
+}
+
+func ExampleWorker() {
+	c := Config{NWorkers: 10, TaskQueueSize: 15}
+
+	//Instantiate a WPExecutor which internally creates a taskqueue and workerchannelpool :
+	wp, err := NewWPExecutor(c)
+	if err != nil {
+		log.Printf("Failed to instantiate WPExecutor")
+	}
+
+	// Submit a task
+	hello := new(Hello)
+	task := workerpool.Task{hello, "DisplayMsg", "Florest"}
+
+	// Execute the task
+	wp.ExecuteTask(task)
+}
